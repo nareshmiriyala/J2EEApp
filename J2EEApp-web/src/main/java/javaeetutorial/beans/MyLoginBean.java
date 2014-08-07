783d6794876ead60a1c3f5b0b60d267f1b99b68d
Naresh Chandra Miriyala <ncmiriyala@gmail.com>
2014-08-07 14:34:07 +1000

    first commit

--- J2EEApp-web/src/main/java/javaeetutorial/beans/MyLoginBean.java
+++ J2EEApp-web/src/main/java/javaeetutorial/beans/MyLoginBean.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+
 import javax.inject.Named;
 
 @Named
@@ -59,4 +60,5 @@ public class MyLoginBean implements Serializable {
             return "failure";
         }
     }
+  
 }
---
+++ J2EEApp-web/src/main/java/javaeetutorial/searchmovies/ejb/ContactFacade.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2013 Oracle and/or its affiliates. All rights reserved.
+ *
+ * You may not modify, use, reproduce, or distribute this software except in
+ * compliance with  the terms of the License at:
+ * http://java.net/projects/javaeetutorial/pages/BerkeleyLicense
+ */
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaeetutorial.searchmovies.ejb;
+
+
+import com.dellnaresh.youtubemovies.UserDetails;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author ian
+ */
+@Stateless
+public class ContactFacade extends AbstractFacade<UserDetails> {
+    @PersistenceContext(unitName = "persistence")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public ContactFacade() {
+        super(UserDetails.class);
+    }
+
+}
